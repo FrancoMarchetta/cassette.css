5cb1a9761a6f1c05a60e7c6f7898383b77281dae
FrancoMarchetta <frncmarchetta@gmail.com>
2026-05-07 17:51:14 +0000

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://FrancoMarchetta.github.io',
+  base: '/cassette.css/',
   vite: {
     plugins: [tailwindcss()]
   }
